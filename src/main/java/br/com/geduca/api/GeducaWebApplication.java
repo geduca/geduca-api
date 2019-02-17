4feddcd9c7c07aeaff764da1c1a1606edd94d549
Gustavo Clay <gustavoclay@gmail.com>
2019-02-17 01:01:52 -0300

    Sprint Zero - Setup do projeto

---
+++ src/main/java/br/com/geduca/api/GeducaWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.geduca.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeducaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeducaWebApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/geduca/api/model/Pessoa.java
@@ -0,0 +1,16 @@
+package br.com.geduca.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pessoa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	
+}
