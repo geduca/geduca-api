c59f021d57783e2ec577a800bca84f22f69d8b46
Gustavo Clay <gustavoclay@gmail.com>
2019-02-17 23:44:17 -0300

    Implementação inicial da classe pessoa, continuar

--- src/main/java/br/com/geduca/api/model/Pessoa.java
+++ src/main/java/br/com/geduca/api/model/Pessoa.java
@@ -1,16 +1,38 @@
 package br.com.geduca.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import br.com.geduca.api.converters.TipoPessoaConverter;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author gustavoclay
+ * 
+ *         Classe Pessoa
+ */
+@Setter
+@Getter
 @Entity
 public class Pessoa {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_pessoa", updatable = false)
 	private Long codigo;
+
+	@Column(name = "cpf_cnpj")
+	private String cpfCpnj;
+
+	@Convert(converter = TipoPessoaConverter.class)
+	@Column(name = "tipo_pessoa")
+	private TipoPessoa tipoPessoa;
 	
-	
+	// TODO: CONTINUAR A IMPLEMENTAÇÃO DA CLASSE PESSOA
+
 }
