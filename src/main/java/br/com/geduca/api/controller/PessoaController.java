ab9eff84d0568c28803ed0bd73d000705110e842
Mateus Henrique <mateus_twister@hotmail.com>
2019-03-05 21:59:19 -0300

    tratamento de erros, isolar acesso aos dados

--- src/main/java/br/com/geduca/api/controller/PessoaController.java
+++ src/main/java/br/com/geduca/api/controller/PessoaController.java
@@ -1,11 +1,12 @@
 package br.com.geduca.api.controller;
 
+import java.util.Optional;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,11 +17,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.geduca.api.model.Pessoa;
-import br.com.geduca.api.repository.PessoaRepository;
 import br.com.geduca.api.service.PessoaService;
 
 @RestController
@@ -30,43 +29,64 @@ public class PessoaController {
 	@Autowired
 	private PessoaService pessoaService;
 
-	@Autowired
-	private PessoaRepository pessoaRepository;
-
 	@GetMapping
 	public Page<Pessoa> pesquisar(@RequestParam(required = false, defaultValue = "%") String nome,
 			@RequestParam int pagina, @RequestParam int max) {
-		return pessoaRepository.findByNomeContaining(nome, PageRequest.of(pagina, max));
+		return pessoaService.findByNomeContaining(nome, pagina, max);
 	}
 
 	@PostMapping
-	public ResponseEntity<Pessoa> criar(@RequestBody Pessoa pessoa, HttpServletResponse response) {
-		Pessoa pessoaSalva = pessoaService.salvar(pessoa);
-		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
+	public ResponseEntity<String> criar(@RequestBody Pessoa pessoa, HttpServletResponse response) {
+		try {
+			pessoaService.salvar(pessoa);
+
+		} catch (Exception e) {
+			return new ResponseEntity<String>("Erro ao adicionar a pessoa" + pessoa.getNome(),
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body("Pessoa " + pessoa.getNome() + " adicionada com sucesso !");
 	}
 
 	@GetMapping("/{codigo}")
-	public ResponseEntity<Pessoa> buscaPeloCodigo(@PathVariable long codigo) {
-		Pessoa pessoa = pessoaRepository.getOne(codigo);
-		return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
+	public ResponseEntity<Pessoa> buscaPeloCodigo(@PathVariable Long codigo) {
+		Optional<Pessoa> pessoa =  pessoaService.findById(codigo);
+		if (pessoa.isPresent()) {
+			return ResponseEntity.ok(pessoa.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 
 	@DeleteMapping("/{codigo}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void remover(@PathVariable Long codigo) {
-		pessoaRepository.deleteById(codigo);
+	public ResponseEntity<String> remover(@PathVariable Long codigo) {
+		try {
+			pessoaService.deleteById(codigo);
+		} catch (Exception e) {
+			return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return ResponseEntity.status(HttpStatus.OK).body("Pessoa de código" + codigo + " deletada com sucesso !");
 	}
 
 	@PutMapping("/{codigo}")
 	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
-		Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
-		return ResponseEntity.ok(pessoaSalva);
+		try {
+			pessoaService.atualizar(codigo, pessoa);
+		} catch (Exception e) {
+			return new ResponseEntity<Pessoa>(pessoa, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return ResponseEntity.ok(pessoa);
 	}
 
 	@PutMapping("/{codigo}/ativo")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
-		pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
+	public ResponseEntity<String> atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
+		try {
+			pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
+
+		} catch (Exception e) {
+			return new ResponseEntity<String>("Erro ao atualizar", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return ResponseEntity.ok("Atualizado com sucesso !");
 	}
 
 }
