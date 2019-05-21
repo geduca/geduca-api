46419bbc353f85a323ba1a2ec5585f059f989fe0
Gustavo Clay <gustavoclay@gmail.com>
2019-05-21 01:20:45 -0300

    ajuste inicial mudança para o postgresql, continuar'

--- src/main/java/br/com/geduca/api/GeducaWebApplication.java
+++ src/main/java/br/com/geduca/api/GeducaWebApplication.java
@@ -3,6 +3,7 @@ package br.com.geduca.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Classe bootstrap do Spring
@@ -11,6 +12,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
  *
  */
 @SpringBootApplication
+@EntityScan(basePackages = { "br.com.geduca.api.model" })
+@EnableJpaRepositories(basePackages = { "br.com.geduca.api.repository" })
 public class GeducaWebApplication {
 
 	public static void main(String[] args) {
